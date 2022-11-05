3612f52c0ffcb45d88cb6f4601a8b985ce382124
khayalfarzi <khayal.farziyev@pashabank.az>
2022-11-05 19:37:42 +0400

    lesson 12 exercise

--- src/main/java/az/iktlab/lessons/lesson12/MethodExercises.java
+++ src/main/java/az/iktlab/lessons/lesson12/MethodExercises.java
@@ -3,8 +3,16 @@ package az.iktlab.lessons.lesson12;
 public class MethodExercises {
 
     public static boolean isEven(int number) {
+        if (number == 0) return false;
 
-        return false;
+        while (number != 0) {
+
+            if ((number % 10) % 2 != 0)
+                return false;
+
+            number /= 10;
+        }
+        return true;
     }
 
     public static int[] findCommonElements(int[] arr1, int[] arr2) {
