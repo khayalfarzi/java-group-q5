1c6406da551bfea55f260be30412830a91fb3b9a
khayalfarzi <khayal.farziyev@pashabank.az>
2022-11-05 20:13:42 +0400

    lesson 12 exercise

--- src/main/java/az/iktlab/lessons/lesson12/MethodExercises.java
+++ src/main/java/az/iktlab/lessons/lesson12/MethodExercises.java
@@ -1,5 +1,7 @@
 package az.iktlab.lessons.lesson12;
 
+import java.util.Arrays;
+
 public class MethodExercises {
 
     public static boolean isEven(int number) {
@@ -15,8 +17,54 @@ public class MethodExercises {
         return true;
     }
 
-    public static int[] findCommonElements(int[] arr1, int[] arr2) {
+    public static String findCommonElements(int[] arr1, int[] arr2) {
+        int length = Math.min(arr1.length, arr2.length);
+        int[] newArr = new int[length];
+        int index = 0;
 
-        return new int[]{};
+        for (int elem : arr1) {
+            if (isExist(elem, arr2)) {
+                newArr[index] = elem;
+                index++;
+            }
+        }
+
+        /**
+         *  arr1 = [1, 2, 3, 4]
+         *  arr2 = [2, 3, 4, 5, 6]
+         *
+         *  newArr = [2, 3, 4, 0]
+         * */
+        Arrays.sort(newArr);
+
+        return Arrays.toString(cleanUpZeros(newArr));
+    }
+
+    public static boolean isExist(int element, int[] arr2) {
+        for (int elem : arr2) {
+            if (elem == element)
+                return true;
+        }
+        return false;
+    }
+
+    public static int[] cleanUpZeros(int[] arr) {
+
+        int size = 0;
+        for (int elem : arr) {
+            if (elem != 0)
+                size++; // 3
+        }
+
+        int index = 0;
+        int[] cleanArray = new int[size];
+        for (int elem : arr) {
+            if (elem != 0) {
+                cleanArray[index] = elem;
+                index++;
+            }
+        }
+
+        return cleanArray;
     }
 }
--- src/test/java/az/iktlab/lessons/lesson12/MethodExercisesTest.java
+++ src/test/java/az/iktlab/lessons/lesson12/MethodExercisesTest.java
@@ -3,6 +3,8 @@ package az.iktlab.lessons.lesson12;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static az.iktlab.lessons.lesson12.MethodExercises.findCommonElements;
 
 public class MethodExercisesTest {
@@ -17,11 +19,12 @@ public class MethodExercisesTest {
         Assert.assertTrue(MethodExercises.isEven(460082));
     }
 
+    @Test
     public void findCommonElementsTest() {
-        Assert.assertEquals(new int[]{8, 9}, findCommonElements(new int[]{1, 2, 3, 4, 5, 67, 8, 9}, new int[]{6, 7, 8, 9}));
-        Assert.assertEquals(new int[]{2, 3, 88, 99, 123}, findCommonElements(
+        Assert.assertEquals(Arrays.toString(new int[]{8, 9}), findCommonElements(new int[]{1, 2, 3, 4, 5, 67, 8, 9}, new int[]{6, 7, 8, 9}));
+        Assert.assertEquals(Arrays.toString(new int[]{2, 3, 99, 123}), findCommonElements(
                 new int[]{134, 256, 789, 2, 567, 9864356, 1290, 9841230, 189034, 3, 99, 123},
                 new int[]{2, 98645763, 9542683, 71234, 123, 123, 123, 4567, 2345566, 90, 99, 88, 3}));
-        Assert.assertEquals(new int[]{1, 23, 456, 654}, findCommonElements(new int[]{456, 123, 654, 1, 23, 23, 34, 34, 18, 901}, new int[]{476, 1, 343, 456, 901, 809, 812, 654, 398163, 0, 285}));
+        Assert.assertEquals(Arrays.toString(new int[]{1, 456, 654, 901}), findCommonElements(new int[]{456, 123, 654, 1, 23, 23, 34, 34, 18, 901}, new int[]{476, 1, 343, 456, 901, 809, 812, 654, 398163, 0, 285}));
     }
 }
