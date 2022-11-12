a13b45fb84d815684045d374f086e3485383bfbb
khayalfarzi <khayal.farziyev@pashabank.az>
2022-11-12 13:57:48 +0400

    lesson 13 exercises

--- src/main/java/az/iktlab/lessons/lesson13/ArrayManipulation.java
+++ src/main/java/az/iktlab/lessons/lesson13/ArrayManipulation.java
@@ -1,9 +1,19 @@
 package az.iktlab.lessons.lesson13;
 
+import java.util.Arrays;
+
 public class ArrayManipulation {
 
     public static int findSecondMaxNumber(int[] arr) {
+        Arrays.sort(arr);
+        int length = arr.length;
 
-        return 1;
+        int idealNumber = arr[length - 2];
+        int lastNumber = arr[length - 1];
+
+        if (idealNumber == lastNumber)
+            for (int i = length - 1; i >= 0; i--)
+                if (arr[i] != idealNumber) return arr[i];
+        return idealNumber;
     }
 }
