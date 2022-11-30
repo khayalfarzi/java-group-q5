30311a286804e71488926698345af81d0411f702
khayalfarzi <khayal.farziyev@pashabank.az>
2022-11-30 19:16:38 +0400

    lesson 20: exercise

---
+++ src/main/java/az/iktlab/lessons/lesson20/ListExample.java
@@ -0,0 +1,11 @@
+package az.iktlab.lessons.lesson20;
+
+import java.util.List;
+
+public class ListExample {
+
+    public static int findSecondMaxNumber(List<Integer> arr) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/az/iktlab/lessons/lesson20/ArrayManipulationTest.java
@@ -0,0 +1,21 @@
+package az.iktlab.lessons.lesson20;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static az.iktlab.lessons.lesson20.ListExample.findSecondMaxNumber;
+
+public class ArrayManipulationTest {
+
+    @Test
+    public void findSecondMaxNumberTest() {
+
+        Assert.assertEquals(5, findSecondMaxNumber(List.of(1, 2, 1, 1, 1, 12, 3, 5)));
+        Assert.assertEquals(8, findSecondMaxNumber(List.of(8, 8)));
+        Assert.assertEquals(456, findSecondMaxNumber(List.of(5, 7, 123, 456, 567, 45, 45, 45, 12, 1, 11, 1, 2, 3)));
+        Assert.assertEquals(89, findSecondMaxNumber(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 24, 34, 56, 78, 98, 67, 89)));
+        Assert.assertEquals(0, findSecondMaxNumber(List.of(-2, 3, -4, -5, -6, 0, -78, -98)));
+    }
+}
